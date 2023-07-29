991441b123b9a1a15241a778a3c65142e16190a5
Timothy Marois <timothymarois@gmail.com>
2023-07-29 13:29:05 -0400

    Doc fix for dropdowns

--- docs/components/dropdown/examples/DropdownMenuExample.vue
+++ docs/components/dropdown/examples/DropdownMenuExample.vue
@@ -1,16 +1,16 @@
 <template>
 	<div class="w-full vp-raw p-8 bg-white border-2 border-gray-300 rounded">
 		<Dropdown arrow>
-        <Button>Dropdown Menu</Button>
-        <template #popper>
-          <List>
-              <ListItem>Item 1</ListItem>
-              <ListItem>Item 2</ListItem>
-              <ListItem>Item 3</ListItem>
-              <ListItem>Item 4</ListItem>
-          </List>
-        </template>
-    </Dropdown>
+            <Button>Dropdown Menu</Button>
+            <template #popper>
+            <List>
+                <ListItem>Item 1</ListItem>
+                <ListItem>Item 2</ListItem>
+                <ListItem>Item 3</ListItem>
+                <ListItem>Item 4</ListItem>
+            </List>
+            </template>
+        </Dropdown>
 	</div>
 </template>
 <script setup>
