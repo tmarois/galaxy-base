39bb52eb1b06ef8efa00b85054e6a439bb77a13b
Timothy Marois <timothymarois@gmail.com>
2023-09-21 15:18:19 -0400

    Updated list item for links

--- src/components/Elements/List/ListItem.vue
+++ src/components/Elements/List/ListItem.vue
@@ -1,15 +1,18 @@
 <template>
-    <div :class="{
-        'flex items-center': true,
-        'list--item': props.hover && !props.disabled && !props.divider,
-        'h-[1px]': props.divider,
-        [variantClasses.listItem]: !props.divider,
-        [variantClasses.listItemSize]: !props.divider,
-        [variantClasses.listItemTheme]: !props.disabled || !props.divider,
-        [variantClasses.listItemHover]: props.hover && !props.disabled && !props.divider,
-        [variantClasses.listItemDisabled]: props.disabled && !props.divider,
-        [variantClasses.listItemDivider]: props.divider
-    }">
+    <component 
+        v-bind="options.data" 
+        :is="options.tag" 
+        :class="{
+            'flex items-center': true,
+            'list--item': props.hover && !props.disabled && !props.divider,
+            'h-[1px]': props.divider,
+            [variantClasses.listItem]: !props.divider,
+            [variantClasses.listItemSize]: !props.divider,
+            [variantClasses.listItemTheme]: !props.disabled || !props.divider,
+            [variantClasses.listItemHover]: props.hover && !props.disabled && !props.divider,
+            [variantClasses.listItemDisabled]: props.disabled && !props.divider,
+            [variantClasses.listItemDivider]: props.divider
+        }">
         <div v-if="slots?.before" :class="[variantClasses.listItemBefore]">
             <slot name="before"></slot>
         </div>
@@ -19,7 +22,7 @@
         <div v-if="slots['after']" :class="[variantClasses.listItemAfter]">
             <slot name="after"></slot>
         </div>
-    </div>
+    </component>
 </template>
 
 <script setup>
@@ -48,6 +51,22 @@ const props = defineProps({
         type: Boolean,
         default: false
     },
+    target: {
+        type: String,
+        default: null
+    },
+    to: {
+        type: String,
+        default: null
+    },
+    href: {
+        type: String,
+        default: null
+    },
+    tag: {
+        type: String,
+        default: 'div'
+    },
 });
 
 const isGrouped = inject('IS_LIST_GROUP', 0);
@@ -57,6 +76,33 @@ const listItemVariant = ref(props.variant);
 if (isGrouped && isGroupVariant && !props.variant) {
     listItemVariant.value = isGroupVariant;
 }
+
+const options = computed(() => {
+    let tag = '';
+
+    const data = {
+        target: null,
+        href: null,
+        to: null
+    };
+
+    if (!props.disabled) {
+        if (props.to) {
+            tag = 'RouterLink';
+            data.to = props.to;
+        } 
+        else {
+            tag = (props.href && 'a') || props.tag || 'div';
+            if (tag === 'a' && props.href) data.href = props.href;
+        }
+        if (props.target) data.target = props.target;
+    }
+    else {
+        tag = 'div'
+    }
+
+    return { tag, data };
+});
 
 const variantClasses = useVariantClasses('List', listItemVariant.value, props.classes);
 </script>
